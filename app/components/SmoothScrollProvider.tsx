da39947ee34dd972cb56c5f46eb50a9521d830d2
md-sahil-alam <mdsahil.prof@gmail.com>
2026-01-04 23:59:40 +0530

    ass comonent

---
+++ app/components/SmoothScrollProvider.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
+
+export default function SmoothScrollProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      smoothWheel: true,
+      lerp: 0.06, // lower = smoother
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+}
